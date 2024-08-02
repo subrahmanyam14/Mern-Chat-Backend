e82972c96fbc78a4ad5d6fdb50b37f28dba30d37
subrahmanyam14 <139866081+subrahmanyam14@users.noreply.github.com>
2024-08-02 10:15:09 +0530

    Update socket.js

--- socket/socket.js
+++ socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173","http://localhost:5174"],
+        origin: ["http://localhost:5173","https://mern-chat-application-sage.vercel.app"],
         methods: ["GET", "POST", "DELETE"],
     },
 });
